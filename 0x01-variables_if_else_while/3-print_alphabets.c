5139ed27f227141718c8c1b3f985143340cd06ec
Peace Ihendi <peaceihendi@gmail.com>
2023-02-17 19:14:45 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower and upper case
+ * Return: 0 (success)
+ */
+int main(void)
+{
+<<<<<<< HEAD
+        char ch;
+
+  foCXXCr (ch = 'a'; ch <= 'z'; ch++)
+    putchar(ch);
+  for (ch = 'A'; ch <= 'Z'; ch++)
+    putchar(ch);
+  putchar('\n');
+  return (0);
+=======
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+>>>>>>> c474bfaeb52d3c209c6b8ceea579a475c96d0a04
+}
