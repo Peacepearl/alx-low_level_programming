6301470d7e572aa375ce04d9f802f0609829e9d4
Peace Ihendi <peaceihendi@gmail.com>
2023-02-21 13:27:18 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
-#include "main.h"
-
+#include "main.h" /* including a local header file called main.h */
 
 /**
  * main - Entry point
- *
- * Return: nothing, return void
+ * program that prints _putchar
+ * Return: 0 (success)
  */
-
 int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
+{	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
