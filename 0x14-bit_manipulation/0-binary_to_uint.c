8a787b8db87d659514f88393737482238d31f9c5
Peacepearl <peaceihendi@gmail.com>
2023-04-26 03:57:03 -0700

    0 binary unit 3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,14 +6,14 @@
  *
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_unit(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
-	for (i = 0; n[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
